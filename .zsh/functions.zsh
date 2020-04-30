6bdb69118452db1cd43a11a398822c9a06393aec
morishin <sntr92@gmail.com>
2020-04-30 13:09:49 +0900

    Update pre-commit

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -181,6 +181,13 @@ function git-compare() {
   hub browse -- "compare/${1:-master}...$remote:$(git symbolic-ref --short HEAD)?expand=1"
 }
 
+function update-githooks() {
+  if [ -e .git/hooks ]; then
+    rm -rf .git/hooks
+    ln -s ~/.git-template/hooks .git/hooks
+  fi
+}
+
 function morishinzo() {
    envchain morishin aws s3 cp --acl public-read $1 s3://g.morishin.me/ | sed -E 's/.*s3(.*)/https\1/g'
 }
